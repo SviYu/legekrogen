e22931c84a9b18e398a28ebf839f661211998c64
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2023-04-16 11:15:33 +0200

    headerOverlay bg style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Header = ({image, title, subtitle, subtext}) => {
   return (
       <div className='header'>
-          <div className="header-overlay"></div>
+          {/* <div className="header-overlay"></div> */}
           <img src={image} className='header-bg' alt="Children's toys" />
-          <div className="container header-container">
+      <div className="header-container">
+        <div className="container">
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
             <p>{subtext}</p>
-          </div>
+        </div>
+      </div>
      </div>
   )
 }
